f80fa3689038dc0d97303a2c3f64bad88b849251
Matt Farmer <mrfarmer777@gmail.com>
2018-03-31 16:27:08 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(pname)
+    @person_name=person_name
+  end
+  def name
+    puts "#{@person_name}"
+  end
+  def job=(pjob)
+    @person_job=pjob
+  end
+  def job
+    puts "#{@person_job}"
+  end
+  
